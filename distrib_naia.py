11e26deeb4ae69e89dad63965460b44a93de27da
NaIA (Navarra Inteligencia Artificial) <103188903+naia-AI@users.noreply.github.com>
2022-04-08 13:07:05 +0200

    Add files via upload

---
+++ distrib_naia.py
@@ -0,0 +1,36 @@
+from fastai.vision.all import *
+from fastai.distributed import *
+from fastai.vision.models.xresnet import *
+import argparse
+
+# Cell
+def rank0_first(func, *args, **kwargs):
+    "Execute `func` in the Rank-0 process first, then in other ranks in parallel."
+    if args: func = partial(func,*args)
+    dummy_l = Learner(DataLoaders(device='cpu'), nn.Linear(1,1), loss_func=lambda: 0)
+    with dummy_l.distrib_ctx(cuda_id=int(kwargs['cuda_id'])):
+        print(rank_distrib())
+        if not rank_distrib(): res = func()
+        distrib_barrier()
+        if rank_distrib(): res = func()
+    return res
+    
+  
+@call_parse
+def main(
+    args:Param("Args to pass to script", nargs='...', opt=False)=''
+):  
+   
+    os.environ["NCCL_SOCKET_IFNAME"]=args[1]
+    
+    path = rank0_first(untar_data, URLs.IMAGEWOOF_320,cuda_id=int(args[0]))
+    dls = DataBlock(
+        blocks=(ImageBlock, CategoryBlock),
+        splitter=GrandparentSplitter(valid_name='val'),
+        get_items=get_image_files, get_y=parent_label,
+        item_tfms=[RandomResizedCrop(160), FlipItem(0.5)],
+        batch_tfms=Normalize.from_stats(*imagenet_stats)
+    ).dataloaders(path, path=path, bs=64)
+
+    learn = Learner(dls, xresnet50(n_out=10), metrics=[accuracy,top_k_accuracy]).to_fp16()
+    with learn.distrib_ctx(cuda_id=int(args[0])): learn.fit_flat_cos(2, 1e-3, cbs=MixUp(0.1))
---
+++ launch_naia.py
@@ -0,0 +1,32 @@
+import subprocess,torch,os,sys
+from fastcore.basics import *
+from fastcore.script import *
+
+@call_parse
+def main(
+    gpus_to_use:Param("The ids of GPUs to use for distributed training in this node. Example: 0 1", str)='all',
+    total_gpus:Param("The total number of GPUs to use for distributed training in all nodes", int)=1,
+    nr:Param("The ranking of the GPUs in this node. Example: 0,1", str)='0',
+    ethernet_adapter:Param("Ethernet Adapter to use for this node", str)='',
+    master_IP:Param("The IP of the master in this DDP", str)='',
+    master_PORT:Param("The PORT of the master in this DDP", str)='',
+    script:Param("Script to run", str, opt=False)='',
+    args:Param("Args to pass to script", nargs='...', opt=False)=''
+):
+    "PyTorch distributed training launch helper that spawns multiple distributed processes"
+    current_env = os.environ.copy()
+    gpus_to_use_list = list(range(torch.cuda.device_count())) if gpus_to_use=='all' else gpus_to_use.split(',')
+    gpus_node = torch.cuda.device_count() if gpus_to_use=='all' else len(gpus_to_use.split(','))
+    rankings =  nr.split(',')
+    current_env["WORLD_SIZE"] = str(total_gpus)
+    current_env["MASTER_ADDR"] = master_IP
+    current_env["MASTER_PORT"] = master_PORT
+    
+    if(gpus_node!=len(rankings)):
+        print('ERROR: The number of GPUs to use in this node ('+str(gpus_node)+') does not match with the number of ranking ids introduced (--nr='+str(rankings)+')')
+        exit(-1)
+    procs = []
+    for i,gpu in enumerate(gpus_to_use_list):
+        current_env["RANK"],current_env["DEFAULT_GPU"] = str(rankings[i]),str(gpu)
+        procs.append(subprocess.Popen([sys.executable, "-u", script] + list(str(gpu))+ethernet_adapter.split(' '), env=current_env))
+    for p in procs: p.wait()
